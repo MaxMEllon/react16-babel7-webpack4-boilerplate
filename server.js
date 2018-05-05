47ecaa1fb72de366bf847f5e390ef0ee7578507d
MaxMEllon <maxmellon1994@gmail.com>
2018-05-05 20:00:55 +0900

    Modify config of webpack-serve

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const serve = require('webpack-serve')
+const history = require('connect-history-api-fallback')
+const convert = require('koa-connect')
+const config = require('./webpack.config.dev')
+
+config.serve = {
+  content: [__dirname],
+  hot: {
+    host: 'localhost',
+    port: 5000,
+  },
+  port: 4000,
+  add: (app, middleware, options) => {
+    app.use(convert(history({})))
+  }
+}
+
+serve({ config })
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,9 +22,6 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              cacheDirectory: true
-            }
           }
         ]
       }
