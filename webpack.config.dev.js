616735e96f85ae9e9f9b27dce191f6dc11a8c5c9
MaxMEllon <maxmellon1994@gmail.com>
2018-05-05 16:45:44 +0900

    :tada: first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { name } = require('./package.json')
+
+module.exports = {
+  cache: true,
+  mode: 'development',
+  entry: ['./src/index'],
+  devtool: 'inline-source-map',
+  output: {
+    path: `${__dirname}/dist/`,
+    filename: 'bundle.[hash].js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial'
+    }
+  },
+  plugins: [
+    new Dotenv({ path: './.env' }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: name,
+      template: path.join(__dirname, 'templates', 'template.ejs')
+    })
+  ],
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  }
+}
